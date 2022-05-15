bc7e712b358472f0b498761544dbfdcf32a2489b
Oleh Hryshko <oleg.grishko@gmail.com>
2022-05-15 16:34:46 +0300

    upd

--- src/main/scala/com/epam/hryshko/Consumer.scala
+++ src/main/scala/com/epam/hryshko/Consumer.scala
@@ -1,12 +1,6 @@
 package com.epam.hryshko
 
-import org.apache.kafka.clients.consumer.{ConsumerRecords, KafkaConsumer}
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.streaming.{Duration, StreamingContext}
-import org.apache.spark._
-import org.apache.spark.streaming._
-
-import java.util.Properties
 import scala.io.Source
 
 object Consumer {
@@ -18,17 +12,20 @@ object Consumer {
     censored.getLines().foreach{ word => arrCensored :+= word }
     censored.close()
 
-//    val spark = SparkSession
-//      .builder
-//      .appName("Spark-Kafka-Integration")
-//      .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.13:3.2.1")
-//      .getOrCreate()
 
     val spark = SparkSession
       .builder
       .appName("Spark-Kafka-Integration")
       .master("local")
+//      .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.13:3.2.1")
       .getOrCreate()
+
+
+//    val spark = SparkSession
+//      .builder
+//      .appName("Spark-Kafka-Integration")
+//      .master("local")
+//      .getOrCreate()
 
     val df = spark.readStream
       .format("kafka")
